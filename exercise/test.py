5387cba2bf0ff2489019447e4d431bb10d507179
acgnLiu <liu_wenle_acgn@163.com>
2020-12-09 19:19:02 +0800

    测试

--- exercise/test.py
+++ exercise/test.py
@@ -5,7 +5,7 @@
 # str.rstrip()  #去除字符串右边的空格
 str_01 = "You cannot count on anyone except yourself."
 a = str_01.split(" ")
-print(a[-1])
-b = str_01.split(" ", 3)
-print(b[-1].strip("o"))
+# print(a[-1])
+# b = str_01.split(" ", 3)
+# print(b[-1].strip("o"))
 
